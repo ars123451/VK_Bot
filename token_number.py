a0d8205829a1cce1c882a27282c6aea1a7e4c356
AAA <alextun5@mail.ru>
2023-06-06 20:41:21 +0300

    Add color text

--- token_number.py
+++ token_number.py
@@ -1,8 +1,3 @@
-token = 'vk1.a.9uEFthq_vWULcDaAOCH1--w2zLFTjqIYBDPRMy1kw6iQHXgMM6iiTCa07xPgihELsVaTVdRYyh95m3NH' \
-        '1ipOu0H40sM_UoId7bwFMgxmz5tuozfMFVehBgm5uihy1vcIlGlpdcN_GuLfEQSA7nbb5eJcKcb1vjf1Q9KRW5' \
-        'crHCLkjclddoSX6h1ucwcO8G-gY7vYoGUqnv1Fekmi2aN3zw'
-
-
 token_1 = 'vk1.a.iD9lvBPjTVFv1C0Qz8gt72Z2m8ff8AamuSaizA80gHIpViAWrDjwNUhbIthl5y7VB1QOPcb5mUB' \
           'T8S-pNpD2sjp-nTRWIVSHwTB7lSe61Rf4MIcgoMTRGafluCnz-uvfi7rz00OmnmPC1Yq0_LFpQcd115a5' \
           '3I26weTpW_FiyI-csI0pBrxnXSuH34vdjDmX888RPLce00oVqPJKr3Ds6g'
